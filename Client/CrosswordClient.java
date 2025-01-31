d09a0ac846c3120b95b90fb0c40e0e7a8cb1fe0e
TinkerTinn <61946820+TinkerTinn@users.noreply.github.com>
2025-01-31 11:45:25 -0700

    Complete Interface and Partial Client
    Interface still needs documentation and more testing. Expect debugging. Client still needs to implement the game function. It needs documentation and debugging as well.

--- Client/CrosswordClient.java
+++ Client/CrosswordClient.java
@@ -1,124 +1,192 @@
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-public class CrosswordClient {	
-	private static final String USAGE = 
-	"java CrosswordClient [host] [port] "
-	+ "\n or \n"
-	+ "java CrosswordClient";
-	
+public class CrosswordClient {
+	private static final String USAGE = "java CrosswordClient";
+
+	private static final String MENU_UI = "\n" + "START\n" + "HISTORY\n" + "LOOKUP\n" + "ADD\n" + "REMOVE\n";
+
+	private static final String INVALID_CMD_ERR = "Invalid Command.";
+	private static final String AUTH_ERR = "Incorrect or Invalid Username or Password. Please Try Again.";
+	private static final String ATTEMPTS_ERR = "Max attempts reached.";
+	private static final String RESPONSE_ERR = "Error Getting Response.";
+	private static final String GENERIC_ERR = "An error has occured.";
+
+	private static final String LOGIN_USER_CMD = "LOGIN";
+	private static final String ADD_USER_CMD = "NEW";
+
+	private static final String START_GAME_CMD = "START";
+	private static final String HISTORY_CMD = "HISTORY";
+
+	private static final String LOOKUP_WORD_CMD = "LOOKUP";
+	private static final String ADD_WORD_CMD = "ADD";
+	private static final String REMOVE_WORD_CMD = "REMOVE";
+
+	private static final String END_GAME_CMD = "#";
+	private static final String RESTART_GAME_CMD = "!";
+	private static final String CHECK_SCORE_CMD = "$";
+	private static final String CHECK_WORD_CMD = "?";
+	private static final String WORD_REGEX = "[Aa-zA-Z]+";
+
+	private static final String QUIT_CMD = "QUIT";
+
 	private static final int BUFFER_LIMIT = 1000;
-	
-	private Socket clientSocket;
-	
 
-	public CrosswordClient(String host, int port) {
-		System.out.println("\nConnecting to Server . . .");
-		try {
-			clientSocket = new Socket(host, port);
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		System.out.println("\nConnected.");
-	}
-	
-	void clientToServer(String request) {
-		try {
-			// Create output streams & write the request to the server
-			PrintStream out = 
-				new PrintStream(clientSocket.getOutputStream());
-			out.println (request.toUpperCase());
-			out.println();
-		} catch (IOException i) {
-			i.printStackTrace();
-			System.exit(1);
-		}
+	private static final int INTERFACE_PORT = 69;
+
+	private static final String INTERFACE_HOST = "localhost";
+
+	private static final int QUIT_STATE = 0;
+	private static final int LOGIN_STATE = 1;
+	private static final int MENU_STATE = 2;
+	private static final int GAME_STATE = 3;
+
+	public static void main(String[] args) {
+		CrosswordClient instance = new CrosswordClient();
+		instance.accessServer();
 	}
 
-	String[] serverToClient() {
-		String[] response = new String[BUFFER_LIMIT];
-		
-		try {
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(clientSocket.getInputStream()));
-			int i = 0;
+	static int menu(Scanner userEntry, Scanner fromServer, PrintWriter toServer) {
+
+		while (true) {
+			System.out.print(MENU_UI);
+			String cmd, response;
+
 			do {
-				response[i] = in.readLine();
-				i++;	
-			}  while(in.ready());			
-			
-			in.close();
-		} catch (IOException i) {
-			System.out.println(i.getMessage());
-			System.exit(1);
-		}
-		return response;
-	}
-	
-	void responseHandeler(String[] response) {
-		if(response[0] != "QUIT") {
-			
+				cmd = userEntry.nextLine(); // send cmd to server
+			} while (cmd.isBlank());
+			toServer.println(cmd);
+
+			if (cmd.equals(QUIT_CMD)) { // quit check
+				return QUIT_STATE;
+			}
+
+			if (cmd.equals(START_GAME_CMD)) { // start check
+				return GAME_STATE;
+			}
+
+			response = fromServer.nextLine(); // server response
+			System.out.println(response);
 		}
 	}
 
-	public static void main(String[] args) throws IOException {
-        if (args.length != 2 && args.length != 0) {
-            System.out.println(USAGE);
-            System.exit(1);
-        }
+	static int game(Scanner userEntry, Scanner fromServer, PrintWriter toServer) {
 		
-		CrosswordClient client = null;
-		Scanner userInput = null;
-		String query = "";
-		String[] response = new String[BUFFER_LIMIT];
-		
-		String serverHost = "localhost";  	
-		int serverPort = 69;				
-		
-        if(args.length == 2) {
-        	serverHost = args[0];
-        	serverPort = Integer.parseInt(args[1]);
-        }
-        
-        try {
-        	client = new CrosswordClient(serverHost, serverPort);
-    		userInput = new Scanner(System.in);
-        }  catch (Exception e) {
-        	System.err.println(e.getMessage());
-        	System.exit(1);
-        }
-    	
-    	while(!query.equals("QUIT") && !response[0].equals("QUIT")) {
-    		try {
-    			response = client.serverToClient();
-    			client.responseHandeler(response);
-    			query = userInput.nextLine();
-    			client.clientToServer(query);
-    		} catch (Exception e) {
-            	System.err.println(e.getMessage());
-            	System.exit(1);
-    		}
-    	}
-    	
-    	try {
-    		userInput.close();
-    		client.clientSocket.close();
-    	} catch (IOException i) {
-        	System.err.println(i.getMessage());
-        	System.exit(1);
-    	} catch(Exception e) {
-        	System.err.println(e.getMessage());
-        	System.exit(1);
-    	}
-    	
+		return MENU_STATE;
+	}
+
+	static int login(Scanner userEntry, Scanner fromServer, PrintWriter toServer) {
+		String message, response, cmd, auth;
+
+		do {
+			response = fromServer.nextLine(); // login prompt
+			System.out.println(response);
+
+			do {
+				cmd = userEntry.nextLine(); // send cmd to server
+			} while (cmd.isBlank());
+			toServer.println(cmd);
+
+			if (cmd.equals(QUIT_CMD)) { // quit check
+				return QUIT_STATE;
+			}
+
+			response = fromServer.nextLine(); // username prompt or error
+			System.out.println(response);
+		} while (response.equals(INVALID_CMD_ERR));
+
+		do {
+			do {
+				message = userEntry.nextLine(); // send username to server to server
+			} while (message.isBlank());
+			toServer.println(message);
+
+			if (message.equals(QUIT_CMD)) { // quit check
+				return QUIT_STATE;
+			}
+
+			response = fromServer.nextLine(); // password prompt
+			System.out.println(response);
+
+			do {
+				message = userEntry.nextLine(); // send password to server to server
+			} while (message.isBlank());
+			toServer.println(message);
+
+			if (message.equals(QUIT_CMD)) { // quit check
+				return QUIT_STATE;
+			}
+
+			auth = fromServer.nextLine(); // error message or welcome
+			System.out.println(auth);
+
+			if (auth.equals(AUTH_ERR)) {
+				if (cmd.equals(LOGIN_USER_CMD)) {
+					response = fromServer.nextLine(); // attempts errors
+					System.out.println(response);
+					if (auth.equals(ATTEMPTS_ERR)) {
+						return QUIT_STATE;
+					}
+				}
+				response = fromServer.nextLine(); // username reprompt
+				System.out.println(response);
+			}
+		} while (auth.equals(AUTH_ERR));
+
+		return MENU_STATE;
+	}
+
+	private static void accessServer() {
+		Socket link = null; // Step 1.
+		System.out.println("Connecting to Server...");
+
+		try {
+			link = new Socket(INTERFACE_HOST, INTERFACE_PORT); // Step 1.
+			System.out.println("\nConnected.");
+
+			Scanner fromServer = new Scanner(link.getInputStream()); // Step 2.
+			PrintWriter toServer = new PrintWriter(link.getOutputStream(), true); // Step 2.
+
+			Scanner userEntry = new Scanner(System.in);
+
+			int state = LOGIN_STATE;
+
+			do {
+				switch (state) {
+				case LOGIN_STATE:
+					state = login(userEntry, fromServer, toServer);
+					break;
+				case MENU_STATE:
+					state = menu(userEntry, fromServer, toServer);
+					break;
+				case GAME_STATE:
+					state = game(userEntry, fromServer, toServer);
+					break;
+				default:
+					state = QUIT_STATE;
+					break;
+				}
+			} while (state != QUIT_STATE);
+
+			userEntry.close();
+			fromServer.close();
+		} catch (IOException i) {
+			// handle exception
+		} finally {
+			try {
+				link.close(); // Step 4.
+			} catch (IOException i) {
+				// handle exception
+			}
+		}
 	}
 }
--- Interface/CrosswordInterface.java
+++ Interface/CrosswordInterface.java
@@ -2,19 +2,46 @@
 import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.Scanner;
 
 import java.io.*;
 
 public class CrosswordInterface {
-	private static final String USAGE = "Usage: java MultithreadReverseEchoServer [port]";
-	private static final String LOGIN_QUERY = "LOGIN  or  NEWACCOUNT";
-	private static final String INVALID_COMMAND = "Invalid Command.";
-	private static final String RESPONSE_ERROR = "Error Getting Response.";
-	private static final String VALIDATE_USER_CMD = "CHECK ";
-	private static final String ADD_USER_CMD = "ADD ";
+	private static final String USAGE = "Usage: java CrosswordInterface";
+	private static final String LOGIN_PROMPT = "LOGIN   OR   NEW   OR   QUIT";
+	private static final String WELCOME_MESSAGE = "WELCOME!";
+
+	private static final String INVALID_CMD_ERR = "Invalid Command.";
+	private static final String AUTH_ERR = "Incorrect or Invalid Username or Password. Please Try Again.";
+	private static final String ATTEMPTS_ERR = "Max attempts reached.";
+	private static final String RESPONSE_ERR = "Error Getting Response.";
+	private static final String GAME_SERVER_ERROR = "Game Disconnect Error.";
+
+	private static final String LOGIN_USER_CMD = "LOGIN";
+	private static final String ADD_USER_CMD = "NEW";
+
+	private static final String START_GAME_CMD = "START";
+	private static final String HISTORY_CMD = "HISTORY";
+
+	private static final String LOOKUP_WORD_CMD = "LOOKUP";
+	private static final String ADD_WORD_CMD = "ADD";
+	private static final String REMOVE_WORD_CMD = "REMOVE";
+
+	private static final String END_GAME_CMD = "#";
+	private static final String RESTART_GAME_CMD = "!";
+	private static final String CHECK_SCORE_CMD = "$";
+	private static final String CHECK_WORD_CMD = "?";
+	private static final String WORD_REGEX = "[Aa-zA-Z]+";
+
+	private static final String LOG_LOSS_CMD = "LOSS";
+	private static final String LOG_WIN_CMD = "WIN";
+
+	private static final String GET_GAME_CMD = "";
+
+	private static final String QUIT_CMD = "QUIT";
 
 	private static final int BUFFER_LIMIT = 1000;
+
+	private static final int INTERFACE_PORT = 69;
 
 	private static final int ACCOUNT_PORT = 6969;
 	private static final int GAME_PORT = 420;
@@ -24,49 +51,55 @@ public class CrosswordInterface {
 	private static final String GAME_HOST = "localhost";
 	private static final String WORD_HOST = "localhost";
 
+	private static final int DEFUALT_STATE = 2; // FOR TESTING
+
 	private static final int QUIT_STATE = 0;
 	private static final int LOGIN_STATE = 1;
 	private static final int MENU_STATE = 2;
 	private static final int GAME_STATE = 3;
 
+	private static final int THREAD_COUNT = 20;
 	private static final int MAX_ATTEMPTS = 10;
 
 	public static void main(String[] args) throws IOException {
-		if (args.length != 1) {
+		if (args.length != 0) {
 			System.err.println(USAGE);
 			System.exit(1);
 		}
 
-		int port = 0;
+		int port = INTERFACE_PORT;
 		ServerSocket server = null;
 
 		try {
-			port = Integer.parseInt(args[0]);
 			server = new ServerSocket(port);
 			System.out.println("The server is running...");
 
-			ExecutorService fixedThreadPool = Executors.newFixedThreadPool(20);
+			ExecutorService fixedThreadPool = Executors.newFixedThreadPool(THREAD_COUNT);
 			while (true) {
 				fixedThreadPool.execute(new CrosswordClientHandler(server.accept()));
 			}
 
-		} catch (IOException e) {
+		} catch (IOException i) {
 			System.out.println(
 					"Exception caught when trying to listen on port " + port + " or listening for a connection");
-			System.out.println(e.getMessage());
+			System.out.println(i.getMessage());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+
+		System.out.println("Server Closed.");
 	}
 
 	private static class CrosswordClientHandler implements Runnable {
 		private Socket clientSocket;
+		private String clientUsername;
 
 		CrosswordClientHandler(Socket socket) {
 			this.clientSocket = socket;
 		}
 
 		String handleUDP(String host, int port, String query) {
+
 			try {
 				// get a datagram socket
 				DatagramSocket socket = new DatagramSocket();
@@ -87,100 +120,191 @@ public class CrosswordInterface {
 				socket.close();
 
 				return packet.toString();
-			} catch (NumberFormatException e) {
+			} catch (NumberFormatException n) {
 				System.err.println("Invalid port number: " + port + ".");
-				System.exit(1);
 			} catch (Exception e) {
 				System.err.println(e.getMessage());
-				System.exit(1);
 			}
 
-			return RESPONSE_ERROR;
+			return RESPONSE_ERR;
+
 		}
 
-		int menu(Scanner in, PrintStream out) {
-			while (true) {
-				String query = in.nextLine();
-				String[] parsedQuery = query.split(" ");
-				switch (parsedQuery[0]) {
-				case "START":
-					return GAME_STATE;
-				case "HISTORY":
-					out.print(handleUDP(ACCOUNT_HOST, ACCOUNT_PORT, query));
-				case "CHECK":
-					out.print(handleUDP(WORD_HOST, WORD_PORT, query));
-				case "ADD":
-					out.print(handleUDP(WORD_HOST, WORD_PORT, query));
-				case "REMOVE":
-					out.print(handleUDP(WORD_HOST, WORD_PORT, query));
-				case "QUIT":
-					return QUIT_STATE;
-				default:
-					out.println(INVALID_COMMAND);
+		int menu(BufferedReader fromUser, PrintStream toUser) {
+			try {
+				while (true) {
+					fromUser.mark(BUFFER_LIMIT);
+					String query = fromUser.readLine();
+					String[] parsedQuery = query.split(" ");
+					switch (parsedQuery[0]) {
+					case START_GAME_CMD:
+						fromUser.reset(); // could do this on client side instead, by sending a duplicate cmd
+						return GAME_STATE;
+					case HISTORY_CMD:
+						toUser.println(handleUDP(ACCOUNT_HOST, ACCOUNT_PORT, query));
+						break;
+					case LOOKUP_WORD_CMD:
+						toUser.println(handleUDP(WORD_HOST, WORD_PORT, query));
+						break;
+					case ADD_WORD_CMD:
+						toUser.println(handleUDP(WORD_HOST, WORD_PORT, query));
+						break;
+					case REMOVE_WORD_CMD:
+						toUser.println(handleUDP(WORD_HOST, WORD_PORT, query));
+						break;
+					case QUIT_CMD:
+						return QUIT_STATE;
+					default:
+						toUser.println(INVALID_CMD_ERR);
+					}
 				}
+			} catch (IOException i) {
+				return QUIT_STATE;
 			}
 		}
 
-		int game(Scanner in, PrintStream out) {
-			while (true) {
-				String query = in.nextLine();
-				if (query.startsWith("?")) {
-					query.replace("?", "? ");
-				}
-				String[] parsedQuery = query.split(" ");
+		int game(BufferedReader fromUser, PrintStream toUser) {
+			Socket link = null;
 
-				switch (parsedQuery[0]) {
-				case "#":
+			try {
+				link = new Socket(GAME_HOST, GAME_PORT);
+
+				BufferedReader fromGame = new BufferedReader(new InputStreamReader(link.getInputStream()));
+				PrintWriter toGame = new PrintWriter(link.getOutputStream());
+
+				String gameSetting = fromUser.readLine(); // initial game set-up
+				toGame.println(gameSetting);
+				String gameState = fromGame.readLine();
+				String response = "";
+
+				while (true) {
+					toUser.println(gameState);
+					toUser.println(response);
+
+					String query = fromUser.readLine();
+					String[] parsedQuery = query.split(" ");
+
+					switch (parsedQuery[0]) {
+					case END_GAME_CMD:
+						handleUDP(ACCOUNT_HOST, ACCOUNT_PORT, LOG_LOSS_CMD);
+						try {
+							link.close();
+						} catch (IOException i) {
+							System.err.println(GAME_SERVER_ERROR);
+						}
+						return MENU_STATE;
+					case RESTART_GAME_CMD:
+						handleUDP(ACCOUNT_HOST, ACCOUNT_PORT, LOG_LOSS_CMD);
+						toGame.println(gameSetting);
+					case CHECK_SCORE_CMD:
+						response = handleUDP(ACCOUNT_HOST, ACCOUNT_PORT, query);
+					case CHECK_WORD_CMD:
+						response = handleUDP(WORD_HOST, WORD_PORT, query);
+					case WORD_REGEX:
+						toGame.println(query);
+						gameState = fromGame.readLine();
+						if (gameState == LOG_WIN_CMD) {
+							handleUDP(ACCOUNT_HOST, ACCOUNT_PORT, LOG_WIN_CMD);
+							try {
+								link.close();
+							} catch (IOException i) {
+								System.err.println(GAME_SERVER_ERROR);
+							}
+							return MENU_STATE;
+						} else if (gameState == LOG_LOSS_CMD) {
+							handleUDP(ACCOUNT_HOST, ACCOUNT_PORT, LOG_LOSS_CMD);
+							try {
+								link.close();
+							} catch (IOException i) {
+								System.err.println(GAME_SERVER_ERROR);
+							}
+							return MENU_STATE;
+						}
+					case QUIT_CMD:
+						handleUDP(ACCOUNT_HOST, ACCOUNT_PORT, LOG_LOSS_CMD);
+						try {
+							link.close(); // Step 4.
+						} catch (IOException i) {
+							System.err.println(GAME_SERVER_ERROR);
+						}
+						return QUIT_STATE;
+					default:
+						response = INVALID_CMD_ERR;
+					}
+
+					toGame.println();
+				}
+			} catch (IOException i) {
+				System.err.println(i.getMessage());
+			} finally {
+				try {
+					link.close(); // Step 4.
+				} catch (IOException i) {
+					System.out.println("Error: " + i.getMessage());
 					return MENU_STATE;
-				case "!":
-
-				case "$":
-
-				case "?":
-
-				case "QUIT":
-					return QUIT_STATE;
-				default:
-					out.println(INVALID_COMMAND);
 				}
-			}
-
-		}
-
-		Boolean login(Scanner in, PrintStream out) {
-
-			String host = ACCOUNT_HOST;
-			int port = ACCOUNT_PORT;
-
-			Boolean valid = false;
-
-			int attempts = 0;
-
-			out.println(LOGIN_QUERY);
-			String response = in.nextLine();
-
-			while (!response.equals("LOGIN") && !response.equals("NEWACCOUNT") && !response.equals("QUIT")) {
-				out.println(INVALID_COMMAND);
 			}
 
 			try {
-				// get a datagram socket
+				link.close(); // Step 4.
+				return MENU_STATE;
+			} catch (IOException i) {
+				System.out.println("Error: " + i.getMessage());
+				return MENU_STATE;
+			}
+
+		}
+
+		int login(BufferedReader fromUser, PrintStream toUser) {
+			String host = ACCOUNT_HOST;
+			int port = ACCOUNT_PORT;
+
+			int valid = LOGIN_STATE;
+
+			int attempts = 0;
+
+			try {
+				String clientCMD;
+
+				do {
+					toUser.println(LOGIN_PROMPT);
+					clientCMD = fromUser.readLine();
+					if (clientCMD.equals(QUIT_CMD)) {
+						return QUIT_STATE;
+					}
+
+					if (!clientCMD.equals(LOGIN_USER_CMD) && !clientCMD.equals(ADD_USER_CMD)) {
+						toUser.println(INVALID_CMD_ERR);
+					}
+
+				} while (!clientCMD.equals(LOGIN_USER_CMD) && !clientCMD.equals(ADD_USER_CMD));
+
 				DatagramSocket socket = new DatagramSocket();
 
-				out.print("Username: ");
-				String username = in.nextLine();
+				String username;
+				String password;
 
-				out.print("Password: ");
-				String password = in.nextLine();
+				do {
+					toUser.println("Username: ");
+					username = fromUser.readLine();
 
-				while (!valid && attempts < MAX_ATTEMPTS) {
+					if (username.equals(QUIT_CMD)) {
+						return QUIT_STATE;
+					}
 
-					// send request
+					toUser.println("Password: ");
+					password = fromUser.readLine();
+
+					if (password.equals(QUIT_CMD)) {
+						return QUIT_STATE;
+					}
+
 					byte[] userBuf = new byte[BUFFER_LIMIT];
-					if (response.equals("LOGIN")) {
-						userBuf = (VALIDATE_USER_CMD + username + password).getBytes();
-					} else if (response.equals("NEWACCOUNT")) {
-						userBuf = (ADD_USER_CMD + username + password).getBytes();
+
+					if (clientCMD.equals(LOGIN_USER_CMD)) {
+						userBuf = (LOGIN_USER_CMD + " " + username + " " + password).getBytes();
+					} else if (clientCMD.equals(ADD_USER_CMD)) {
+						userBuf = (ADD_USER_CMD + " " + username + " " + password).getBytes();
 					}
 
 					InetAddress address = InetAddress.getByName(host);
@@ -193,26 +317,38 @@ public class CrosswordInterface {
 					socket.receive(packet);
 
 					if (Boolean.parseBoolean(packet.toString())) {
-						valid = true;
+						valid = MENU_STATE;
+						clientUsername = username;
+						toUser.println(WELCOME_MESSAGE);
 					}
 
-					if (!valid) {
-						out.println(packet.toString());
-						if(response.equals("LOGIN")) {
+					if (valid == LOGIN_STATE) {
+						toUser.println(AUTH_ERR);
+						if (clientCMD.equals(LOGIN_USER_CMD)) {
 							attempts++;
-							out.println(attempts + "attempts left. \n");
+							if (attempts <= MAX_ATTEMPTS) {
+								toUser.println((MAX_ATTEMPTS - attempts) + " attempts left.");
+							}
 						}
 					}
+				} while (valid == LOGIN_STATE && attempts <= MAX_ATTEMPTS);
+
+				if (valid == LOGIN_STATE) {
+					toUser.println(ATTEMPTS_ERR);
+					valid = QUIT_STATE;
 				}
 
 				socket.close();
 
-			} catch (NumberFormatException e) {
+			} catch (NumberFormatException n) {
 				System.err.println("Invalid port number: " + port + ".");
-				System.exit(1);
+				return QUIT_STATE;
+			} catch (IOException i) {
+				System.out.println("Error: " + i.getMessage());
+				return QUIT_STATE;
 			} catch (Exception e) {
-				System.err.println(e.getMessage());
-				System.exit(1);
+				System.err.println("Error: " + e.getMessage());
+				return QUIT_STATE;
 			}
 
 			return valid;
@@ -222,31 +358,29 @@ public class CrosswordInterface {
 		public void run() {
 			System.out.println("Connected, handling new client: " + clientSocket);
 			try {
-				PrintStream userOut = new PrintStream(clientSocket.getOutputStream());
-				Scanner userIn = new Scanner(new InputStreamReader(clientSocket.getInputStream()));
+				PrintStream toUser = new PrintStream(clientSocket.getOutputStream());
+				BufferedReader fromUser = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-				int state = LOGIN_STATE;
-				Boolean authentication = false;
+				int state = DEFUALT_STATE;
 
 				do {
 					switch (state) {
 					case LOGIN_STATE:
-						authentication = login(userIn, userOut);
-						state = MENU_STATE;
+						state = login(fromUser, toUser);
 						break;
 					case MENU_STATE:
-						state = menu(userIn, userOut);
+						state = menu(fromUser, toUser);
 						break;
 					case GAME_STATE:
-						state = game(userIn, userOut);
+						state = game(fromUser, toUser);
 					default:
-						state = 0;
+						state = QUIT_STATE;
 						break;
 					}
-				} while (authentication && state != QUIT_STATE);
+				} while (state != QUIT_STATE);
 
-				userIn.close();
-				userOut.close();
+				fromUser.close();
+				toUser.close();
 
 			} catch (SocketException e) {
 				System.out.println("Error: " + e.getMessage());
@@ -255,7 +389,7 @@ public class CrosswordInterface {
 			} finally {
 				try {
 					clientSocket.close();
-				} catch (IOException e) {
+				} catch (IOException i) {
 
 				}
 				System.out.println("Closed: " + clientSocket);
